6f22f2cadb8c9e4ad4358f0047bf18b1f639d705
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-09 23:24:52 -0500

    added CRUD for all classes

--- server/app.py
+++ server/app.py
@@ -21,12 +21,11 @@ def index():
 
 @app.before_request        #allows any users to see all dogs and login 
 def check_if_logged_in():
-    allowed_endpoints = ['dogs', 'login', 'logout' ]
+    allowed_endpoints = ['dogs', 'login', 'logout', 'trainers' ]
     
     if not session['user_id'] \
         and request.endpoint not in allowed_endpoints :
         return {'error': 'Unauthorized, Please Login'}, 401
-
 
 
 class Dogs(Resource):
@@ -64,12 +63,125 @@ class DogsById(Resource):
             return make_response({}, 204)
         return make_response({'error': 'Dog not found'}, 404)
     
+    def patch(self, id):
+        dog = Dog.query.filter(Dog.id == id).first()
+        if dog:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(dog, attr, data[attr])
+                db.session.commit()
+                return make_response(dog.to_dict(rules=('-classes',)), 202)
+            except ValueError:
+                return make_response({'error': 'Failed to edit dog'}, 400)
+            
+class Trainers(Resource):
+    def get(self):  
+        trainers = [t.to_dict(rules=('-classes',)) for t in Trainer.query.all()]
+        return make_response(trainers, 200)
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            new_trainer = Trainer(
+                name= data['name'],
+                price= data['price']
+                )
+            db.session.add(new_trainer)
+            db.session.commit()
+            return make_response(new_trainer.to_dict(rules=('-classes',)), 201)
+        except ValueError:
+            return make_response({'error': 'Failed to add new trainer, try again!'}, 400)
+
+class TrainersById(Resource):
+    def get(self, id):
+        trainer = Trainer.query.filter(Trainer.id == id).first()
+        if trainer:
+            return make_response(trainer.to_dict(), 200)
+        return make_response({'error': 'Trainer not found'},404)
+    
+    def delete(self, id):
+        trainer = Trainer.query.filter(Trainer.id == id).first()
+        if trainer:
+            db.session.delete(trainer)
+            db.session.commit()
+            return make_response({}, 204)
+        return make_response({'error': 'Trainer not found'}, 404)
+    
+    def patch(self, id):
+        trainer = Trainer.query.filter(Trainer.id == id).first()
+        if trainer:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(Trainer, attr, data[attr])
+                db.session.commit()
+                return make_response(trainer.to_dict(rules=('-classes',)), 202)
+            except ValueError:
+                return make_response({'error': 'Failed to edit trainer'}, 400)
+            
+
+class Classes(Resource):
+    def get(self):  
+        classes = [c.to_dict() for c in Class.query.all()]
+        return make_response(classes, 200)
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            new_class = Class(
+                name= data['name'],
+                dog_id= data['dog_id'],
+                trainer_id= data['trainer_id']
+            )
+            db.session.add(new_class)
+            db.session.commit()
+            return make_response(new_class.to_dict(), 201)
+        except ValueError:
+            return make_response({'error': 'Failed to add new class, try again!'}, 400)
+    
+class ClassesById(Resource):
+    def get(self, id):
+        class_inst = Class.query.filter(Class.id == id).first()
+        if class_inst:
+            return make_response(class_inst.to_dict(), 200)
+        return make_response({'error': 'Class not found'},404)
+    
+    
+    def delete(self, id):
+        class_inst = Class.query.filter(Class.id == id).first()
+        if class_inst:
+            db.session.delete(class_inst)
+            db.session.commit()
+            return make_response({}, 204)
+        return make_response({'error': 'Class not found'}, 404)
+    
+    def patch(self, id):
+        class_inst = Class.query.filter(Class.id == id).first()
+        if class_inst:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(class_inst, attr, data[attr])
+                db.session.commit()
+                return make_response(class_inst.to_dict(), 202)
+            except ValueError:
+                return make_response({'error': 'Failed to edit class'}, 400)
+    
+    
+api.add_resource(Classes, '/classes')   
+api.add_resource(ClassesById, '/classes/<int:id>')
+
+api.add_resource(TrainersById, '/trainers/<int:id>')
+api.add_resource(Trainers, '/trainers', endpoint='trainers')
 api.add_resource(Dogs, '/dogs', endpoint='dogs')
 api.add_resource(DogsById, '/dogs/<int:id>')
 
 
 
 
+
+#authentification
 class CheckSession(Resource):
     def get(self):
         trainer = Trainer.query.filter(Trainer.id == session.get('user_id')).first()
@@ -82,12 +194,15 @@ api.add_resource(CheckSession, '/check_session')
 
 class Login(Resource):
     def post(self):
-        trainer = Trainer.query.filter(
-            Trainer.name == request.get_json()['username']
-        ).first()
-
-        session['user_id'] = trainer.id
-        return trainer.to_dict()
+        try:
+            trainer = Trainer.query.filter(Trainer.name == request.get_json()['username']).first()
+            if trainer:
+                session['user_id'] = trainer.id
+                return make_response(trainer.to_dict(), 201)
+            else:
+                return make_response({'error': 'Invalid username'}, 401)
+        except ValueError:
+            return make_response({'error': 'Login failed'}, 400)
 
 api.add_resource(Login, '/login', endpoint='login')
 
