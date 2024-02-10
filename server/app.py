8dcb99b761a68b45d9b02be7bcb8af0225bb7c18
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-09 19:21:17 -0500

    added login, logout, check session views

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ from config import app, db, api
 # Add your model imports
 from models import Dog, Trainer, Class
 
-
+app.secret_key=b'\x9f\xae\x80\x05q\xe6\xa3EOe\xac\xde#\xf9\xa6\xa7'
 # Views go here!
 
 @app.route('/')
@@ -54,14 +54,69 @@ class DogsById(Resource):
             return make_response({}, 204)
         return make_response({'error': 'Dog not found'}, 404)
     
-            
-        
-    
-        
-
-
 api.add_resource(Dogs, '/dogs')
 api.add_resource(DogsById, '/dogs/<int:id>')
+
+
+
+
+
+
+class CheckSession(Resource):
+    def get(self):
+        trainer = Trainer.query.filter(Trainer.id == session.get('user_id')).first()
+        if trainer:
+            return trainer.to_dict()
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
+class Login(Resource):
+    def post(self):
+        trainer = Trainer.query.filter(
+            Trainer.name == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = trainer.id
+        return trainer.to_dict()
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
