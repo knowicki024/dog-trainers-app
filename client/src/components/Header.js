0571adb6f091eaa5d6c85834617af3a060190199
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-12 22:46:39 -0500

    made navbar links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
-function Header() {
-  return (
-    <header className="header">
-      <Link to="/" className="home-link">
-        <h1>Dog Training Classes</h1>
-      </Link>
-    </header>
-  );
+function Header()
+{
+    return (
+        <header>
+            <nav>
+                <div className="navigation">
+                    <Link
+                        className="nav-button"
+                        to="/">
+                        Home
+                    </Link>
+                    <Link
+                        className="nav-button"
+                        to="/dogs">
+                        Dogs
+                    </Link>
+                    <Link
+                        className="nav-button"
+                        to="/classes">
+                        Classes
+                    </Link>
+                    <Link
+                        className="nav-button"
+                        to="/Trainers">
+                        Trainers
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
-export default Header;
-
+export default Header
